b5f71475da15e673d17e2e0c91e39d2990a42cc0
Charonyuu <bensona1219@gmail.com>
2022-12-21 00:21:46 +0800

    test fix quill require is not defined

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'; // make sure to import it
-
+import babel from 'vite-plugin-babel';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(),babel()],
 });
